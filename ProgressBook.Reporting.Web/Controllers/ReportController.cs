ddd52da606715d0ffe7ebe479e62bad5d82bd0d3
Champion, James <championj@progressbook.com>
2019-12-12 14:09:58 -0500

    Merged PR 3107: Modified exception message, added a conditional check to see if it is a template
    Modified exception message, added a conditional check to see if it is a template
    
    Related work items: #6029

--- ProgressBook.Reporting.Web/Controllers/ReportController.cs
+++ ProgressBook.Reporting.Web/Controllers/ReportController.cs
@@ -66,9 +66,18 @@ namespace ProgressBook.Reporting.Web.Controllers
 
                 if (data == null || data.Length == 0)
                 {
-                    throw new Exception(
-                        "Unable to merge report data with template. Verify report template contains valid bookmarks.");
+                    if (report.IsTemplateOutput)
+                    {
+                        throw new Exception(
+                           $"Unable to merge {report.Name} report data with template. Verify report template contains valid bookmarks.");
+                    }
+                    else
+                    {
+                        throw new Exception(
+                            $"No data returned for {report.Name} report");
+                    }
                 }
+
 
                 var contentType = helper.GetExportContentType(model.ExportType);
                 var fileExtension = helper.GetExportFileExtension(model.ExportType);
